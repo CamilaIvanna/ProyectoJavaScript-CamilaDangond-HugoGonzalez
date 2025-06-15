c104537d3b6c521d349ab46a6a1a0bd5f2d9e24d
Camila Ivanna <camila.dangond.abg@gmail.com>
2025-06-15 03:21:31 -0500

    feat: :sparkles: Se creo el script de recuperarPSw y se creo la función de ocultar el formulario de verificación.
    Se creo el script de recuperarPSw y se creo la función de ocultar el formulario de verificación.

---
+++ js/recuperarPsw.js
@@ -0,0 +1,7 @@
+var formularioUno = document.getElementById("contenedor-formulario-verificarDatos");
+var formularioDos = document.getElementById("contenedor-formulario-crearPsw");
+
+function verificar() {
+    formularioUno.style.display = "none";
+    formularioDos.style.display = "block";
+}
