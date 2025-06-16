07d240c19cafb9661b856a75324098e23a325cd3
Camila Ivanna <camila.dangond.abg@gmail.com>
2025-06-16 07:42:35 -0500

    style: :sparkles: Se maqueto y estilizo recargar cuenta.
    Se maqueto y estilizo recargar cuenta.

---
+++ js/recargar.js
@@ -0,0 +1,36 @@
+const listaBancosColombia = [
+  "Bancolombia",
+  "Banco de Bogotá",
+  "Banco Davivienda",
+  "Banco Popular",
+  "Banco de Occidente",
+  "Banco Agrario",
+  "Banco AV Villas",
+  "Banco Caja Social",
+  "Banco Itaú",
+  "Scotiabank Colpatria",
+  "Banco GNB Sudameris",
+  "Banco BBVA Colombia",
+  "Banco Pichincha",
+  "Banco Falabella",
+  "Banco Finandina",
+  "Banco W",
+  "Banco Serfinanza",
+  "Coomeva Financiera",
+  "Banco Coopcentral",
+  "Nequi",
+  "Daviplata",
+  "Movii",
+  "Lulo Bank",
+  "RappiPay",
+  "Nu Colombia"
+];
+
+const bancoSelect = document.getElementById("banco");
+
+listaBancosColombia.forEach(nombreBanco => {
+    const option = document.createElement("option");
+    option.value = nombreBanco;
+    option.textContent = nombreBanco;
+    bancoSelect.appendChild(option);
+});
